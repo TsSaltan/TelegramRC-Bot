3fd05dcc2377a2eae92d23f99abc33815f04b1aa
Ts.Saltan <TsSaltan@gmail.com>
2020-01-11 15:31:23 +0300

    v 3.0

--- src/main/modules/AppModule.php
+++ src/main/modules/AppModule.php
@@ -6,7 +6,7 @@ use std, gui, framework, main;
 
 class AppModule extends AbstractModule
 {
-    const APP_VERSION = '2.2';
+    const APP_VERSION = '3.0';
     
     /**
      * Время запуска программы 
